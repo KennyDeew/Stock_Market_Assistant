656a02073b897ef7e551d7a6424403707ea18f1c
Мельников И.Е. <melnikov_ie@lgnch.spb.ru>
2025-06-03 11:19:06 +0300

    Добавлена оркестрация .NET Aspire.

---
+++ src/StockMarketAssistant.AppHost/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = DistributedApplication.CreateBuilder(args);
+        var cache = builder.AddRedis("cache");
+
+        //var container = builder.AddDockerfile(
+        //    "gateway", "../backend/gateway/");
+        builder.AddProject<Projects.Gateway_WebApi>("gateway-api");
+        builder.AddProject<Projects.AuthService_WebApi>("authservice-api");
+        builder.AddProject<Projects.AuthService_WebApi>("stockcardservice-api");
+        builder.AddProject<Projects.AuthService_WebApi>("portfolioservice-api");
+        builder.AddProject<Projects.AuthService_WebApi>("notificationservice-api");
+        builder.Build().Run();
+    }
+}
