6000628a49650941872fe320103210fb778e6699
Igor Melnikov <iemelnikov@gmail.com>
2025-06-05 23:16:07 +0300

    feat(AnalyticsService): add initial projects layout

--- src/StockMarketAssistant.AppHost/Program.cs
+++ src/StockMarketAssistant.AppHost/Program.cs
@@ -10,11 +10,12 @@
         var apiAuthService = builder.AddProject<Projects.AuthService_WebApi>("authservice-api");
         var apiStockCardService = builder.AddProject<Projects.StockCardService_WebApi>("stockcardservice-api");
         var apiPortfolioService = builder.AddProject<Projects.PortfolioService_WebApi>("portfolioservice-api");
+        var apiAnalyticsService = builder.AddProject<Projects.AnalyticsService_WebApi>("analyticsservice-api");
         var apiNotificationService = builder.AddProject<Projects.NotificationService_WebApi>("notificationservice-api");
 
         // Добавление ресурсов
         var redis = builder.AddRedis("cache");
-        //var postgres = builder.AddPostgres("postgres").AddDatabase("stockcardsdb");
+        //var postgres = builder.AddPostgres("postgres").AddDatabase("stockcarddb");
         //var container = builder.AddDockerfile("gateway", "../backend/gateway/");
 
         // Связывание ресурсов с проектами
