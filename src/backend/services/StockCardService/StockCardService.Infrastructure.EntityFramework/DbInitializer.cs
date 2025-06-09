197fc8b8d7128867d220641cd17be947a823141c
Заворотный А.А. <orphey@mail.ru>
2025-06-09 22:58:55 +0300

    StockCardService-22 Инициирование БД для StockCardSerice

---
+++ src/backend/services/StockCardService/StockCardService.Infrastructure.EntityFramework/DbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockCardService.Infrastructure.EntityFramework;
+using StockMarketAssistant.StockCardService.Data;
+
+namespace StockCardService.Infrastructure.EntityFramework
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(string connectionString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<StockCardDbContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+            
+            using (var context = new StockCardDbContext(optionsBuilder.Options))
+            {
+                context.Database.Migrate();
+            }
+        }
+    }
+}
--- src/backend/services/StockCardService/StockCardService.Infrastructure.EntityFramework/StockCardDbContextFactory.cs
+++ src/backend/services/StockCardService/StockCardService.Infrastructure.EntityFramework/StockCardDbContextFactory.cs
@@ -1,14 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
-using StockMarketAssistant.StockCardService.Data;
+using Microsoft.EntityFrameworkCore.Design;using StockMarketAssistant.StockCardService.Data;using StockMarketAssistant.StockCardService.Data;
 namespace StockMarketAssistant.StockCardService;
 
 public class StockCardDbContextFactory : IDesignTimeDbContextFactory<StockCardDbContext>
 {
 	public StockCardDbContext CreateDbContext(string[] args)
 	{
-		var optionsBuilder = new DbContextOptionsBuilder<StockCardDbContext>();
-		optionsBuilder.UseNpgsql("Your_Connection_String_Here");
+		var optionsBuilder = new DbContextOptionsBuilder<StockCardDbContext>();		optionsBuilder.UseNpgsql("Your_Connection_String_Here");
 
 		return new StockCardDbContext(optionsBuilder.Options);
 	}
