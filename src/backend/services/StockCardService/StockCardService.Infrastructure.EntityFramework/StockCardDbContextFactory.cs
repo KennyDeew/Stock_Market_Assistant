a5ed3c4c0961a8263c4a440248c3f38f6583924a
Alexander <orphey@mail.ru>
2025-06-12 18:36:30 +0300

    Update src/backend/services/StockCardService/StockCardService.Infrastructure.EntityFramework/StockCardDbContextFactory.cs
    Co-authored-by: iemelnikov <78508363+iemelnikov@users.noreply.github.com>

--- src/backend/services/StockCardService/StockCardService.Infrastructure.EntityFramework/StockCardDbContextFactory.cs
+++ src/backend/services/StockCardService/StockCardService.Infrastructure.EntityFramework/StockCardDbContextFactory.cs
@@ -1,13 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;using StockMarketAssistant.StockCardService.Data;using StockMarketAssistant.StockCardService.Data;
-namespace StockMarketAssistant.StockCardService;
-
-public class StockCardDbContextFactory : IDesignTimeDbContextFactory<StockCardDbContext>
-{
-	public StockCardDbContext CreateDbContext(string[] args)
-	{
-		var optionsBuilder = new DbContextOptionsBuilder<StockCardDbContext>();		optionsBuilder.UseNpgsql("Your_Connection_String_Here");
-
-		return new StockCardDbContext(optionsBuilder.Options);
-	}
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using StockMarketAssistant.StockCardService.Infrastructure.EntityFramework;
+
+using Microsoft.EntityFrameworkCore.Design;
+using StockMarketAssistant.StockCardService.Data;
+using StockMarketAssistant.StockCardService.Data;
+
+namespace StockMarketAssistant.StockCardService;
+
+
+
+public class StockCardDbContextFactory : IDesignTimeDbContextFactory<StockCardDbContext>
+
+{
+
+
+	public StockCardDbContext CreateDbContext(string[] args)
+
+	{
+
+		var optionsBuilder = new DbContextOptionsBuilder<StockCardDbContext>();
+		optionsBuilder.UseNpgsql("Your_Connection_String_Here");
+
+
+
+		return new StockCardDbContext(optionsBuilder.Options);
+
+	}
+
 }
