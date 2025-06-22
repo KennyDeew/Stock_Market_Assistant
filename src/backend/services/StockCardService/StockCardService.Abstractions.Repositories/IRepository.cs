04cf997408dd8e8ff2b36a5c518feb9bb6b6c174
KennyDew <dubrovskiynv@gmail.com>
2025-06-22 23:44:24 +0300

    added IRepository and Repositories (abstract, ShareCard, BondCard and CryptoCard)

---
+++ src/backend/services/StockCardService/StockCardService.Abstractions.Repositories/IRepository.cs
@@ -0,0 +1,50 @@
+﻿using StockCardService.Domain.Entities;
+
+namespace StockCardService.Abstractions.Repositories
+{
+    /// <summary>
+    /// Описания общих методов для всех репозиториев.
+    /// </summary>
+    /// <typeparam name="T"> Тип сущности для репозитория </typeparam>
+    /// <typeparam name="TPrimaryKey"> Тип идентификатора сущности </typeparam>
+    public interface IRepository<T, TPrimaryKey>
+        where T : IEntity<TPrimaryKey>
+    {
+        /// <summary>
+        /// Запросить все сущности в базе.
+        /// </summary>
+        /// <param name="noTracking"> Вызвать с AsNoTracking.</param>
+        /// <returns> IQueryable массив сущностей.</returns>
+        IQueryable<T> GetAll(bool noTracking = false);
+
+        /// <summary>
+        /// Запросить все сущности в базе.
+        /// </summary>
+        /// <param name="cancellationToken"> Токен отмены. </param>
+        /// <param name="asNoTracking"> Вызвать с AsNoTracking. </param>
+        /// <returns> Список сущностей. </returns>
+        Task<List<T>> GetAllAsync(CancellationToken cancellationToken, bool asNoTracking = false);
+
+        /// <summary>
+        /// Получить сущность по Id.
+        /// </summary>
+        /// <param name="id"> Id сущности. </param>
+        /// <returns> Cущность. </returns>
+        T GetById(TPrimaryKey id);
+
+        /// <summary>
+        /// Получить сущность по Id.
+        /// </summary>
+        /// <param name="id"> Id сущности. </param>
+        /// <param name="cancellationToken"></param>
+        /// <returns> Cущность. </returns>
+        Task<T> GetByIdAsync(TPrimaryKey id, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Добавить в базу одну сущность.
+        /// </summary>
+        /// <param name="entity"> Сущность для добавления. </param>
+        /// <returns> Добавленная сущность. </returns>
+        Task<T> AddAsync(T entity);
+    }
+}
---
+++ src/backend/services/StockCardService/StockCardService.Infrastructure.Repositories/Repository.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockCardService.Abstractions.Repositories;
+using StockCardService.Domain.Entities;
+using StockMarketAssistant.StockCardService.Infrastructure.EntityFramework;
+
+namespace StockCardService.Infrastructure.Repositories
+{
+    public abstract class Repository<T, TPrimaryKey> : IRepository<T, TPrimaryKey> where T
+       : class, IEntity<TPrimaryKey>
+    {
+        protected readonly StockCardDbContext Context;
+        private readonly DbSet<T> _entitySet;
+
+        protected Repository(StockCardDbContext context)
+        {
+            Context = context;
+            _entitySet = Context.Set<T>();
+        }
+
+        /// <summary>
+        /// Запросить все сущности в базе.
+        /// </summary>
+        /// <param name="asNoTracking"> Вызвать с AsNoTracking. </param>
+        /// <returns> IQueryable массив сущностей. </returns>
+        public virtual IQueryable<T> GetAll(bool asNoTracking = false)
+        {
+            return asNoTracking ? _entitySet.AsNoTracking() : _entitySet;
+        }
+
+        /// <summary>
+        /// Запросить все сущности в базе.
+        /// </summary>
+        /// <param name="cancellationToken"> Токен отмены </param>
+        /// <param name="asNoTracking"> Вызвать с AsNoTracking. </param>
+        /// <returns> Список сущностей. </returns>
+        public virtual async Task<List<T>> GetAllAsync(CancellationToken cancellationToken, bool asNoTracking = false)
+        {
+            return await GetAll().ToListAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Получить сущность по ID.
+        /// </summary>
+        /// <param name="id"> Id сущности. </param>
+        /// <returns> Cущность. </returns>
+        public virtual T GetById(TPrimaryKey id)
+        {
+            return _entitySet.Find(id);
+        }
+
+        /// <summary>
+        /// Получить сущность по Id.
+        /// </summary>
+        /// <param name="id"> Id сущности. </param>
+        /// <param name="cancellationToken"></param>
+        /// <returns> Cущность. </returns>
+        public virtual async Task<T> GetByIdAsync(TPrimaryKey id, CancellationToken cancellationToken)
+        {
+            return await _entitySet.FindAsync((object)id);
+        }
+
+        /// <summary>
+        /// Добавить в базу одну сущность.
+        /// </summary>
+        /// <param name="entity"> Сущность для добавления. </param>
+        /// <returns> Добавленная сущность. </returns>
+        public virtual async Task<T> AddAsync(T entity)
+        {
+            return (await _entitySet.AddAsync(entity)).Entity;
+        }
+
+    }
+}
