dac9546edc985b1c5a2b689a5a4697608eca4132
KennyDew <dubrovskiynv@gmail.com>
2025-06-23 02:10:04 +0300

    added StartUp.cs

--- src/backend/services/StockCardService/StockCardService.WebApi/Program.cs
+++ src/backend/services/StockCardService/StockCardService.WebApi/Program.cs
@@ -1,4 +1,9 @@
-﻿using StockCardService.Infrastructure.EntityFramework;
+﻿using Microsoft.EntityFrameworkCore;
+using StockCardService.Abstractions.Repositories;
+using StockCardService.Infrastructure.EntityFramework;
+using StockCardService.Infrastructure.Repositories;
+using StockMarketAssistant.StockCardService.Domain.Entities;
+using StockMarketAssistant.StockCardService.Infrastructure.EntityFramework;
 
 namespace StockMarketAssistant.StockCardService.WebApi
 {
@@ -9,12 +14,25 @@ namespace StockMarketAssistant.StockCardService.WebApi
             var builder = WebApplication.CreateBuilder(args);
             builder.AddServiceDefaults();
 
+            //EntityFramework
+            builder.Services.AddDbContext<StockCardDbContext>(options =>
+            {
+                options.UseNpgsql(builder.Configuration.GetConnectionString("StockCardDb"),
+                    optionsBuilder => optionsBuilder.MigrationsAssembly("Infrastructure.EntityFramework"));
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+
+            builder.Services.AddControllers();
+            builder.Services.AddScoped(typeof(IRepository<ShareCard, Guid>), typeof(ShareCardRepository));
+            builder.Services.AddScoped(typeof(IRepository<BondCard, Guid>), typeof(BondCardRepository));
+            builder.Services.AddScoped(typeof(IRepository<CryptoCard, Guid>), typeof(CryptoCardRepository));
+
             var app = builder.Build();
 
             //app.MapGet("/", () => "Hello World!");
 
-            var StockCardServiceConnectionString = builder.Configuration.GetConnectionString("StockCardDb");
-            DbInitializer.Initialize(StockCardServiceConnectionString);
+            //var StockCardServisConnectionString = builder.Configuration.GetConnectionString("StockCardDb");
+            //DbInitializer.Initialize(StockCardServisConnectionString);
             
 
             app.Run();
