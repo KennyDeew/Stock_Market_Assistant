58a32eb1f0285b2f365ed9f2aed931dbd64dcf2f
KennyDew <dubrovskiynv@gmail.com>
2025-06-23 23:48:49 +0300

    setup program remove Startup.cs

--- src/backend/services/StockCardService/StockCardService.WebApi/Program.cs
+++ src/backend/services/StockCardService/StockCardService.WebApi/Program.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using StockCardService.Abstractions.Repositories;
 using StockCardService.Infrastructure.EntityFramework;
 using StockCardService.Infrastructure.Repositories;
 using StockMarketAssistant.StockCardService.Domain.Entities;
 using StockMarketAssistant.StockCardService.Infrastructure.EntityFramework;
+using StockMarketAssistant.StockCardService.WebApi.Helper;
 
 namespace StockMarketAssistant.StockCardService.WebApi
 {
@@ -22,10 +24,12 @@ namespace StockMarketAssistant.StockCardService.WebApi
                 options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
             });
 
+            //IOC
             builder.Services.AddControllers();
             builder.Services.AddScoped(typeof(IRepository<ShareCard, Guid>), typeof(ShareCardRepository));
             builder.Services.AddScoped(typeof(IRepository<BondCard, Guid>), typeof(BondCardRepository));
             builder.Services.AddScoped(typeof(IRepository<CryptoCard, Guid>), typeof(CryptoCardRepository));
+
 
             var app = builder.Build();
 
@@ -33,7 +37,24 @@ namespace StockMarketAssistant.StockCardService.WebApi
 
             //var StockCardServisConnectionString = builder.Configuration.GetConnectionString("StockCardDb");
             //DbInitializer.Initialize(StockCardServisConnectionString);
-            
+
+            // Configure the HTTP request pipeline.
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            //app.UseHttpsRedirection();
+
+
+            app.UseRouting();
+            app.UseCors();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.MigrateDatabase<StockCardDbContext>();
 
             app.Run();
         }
