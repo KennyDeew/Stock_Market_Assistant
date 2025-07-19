8af8fa525f647527990eb3374451fea294fa6af5
Igor Melnikov <iemelnikov@gmail.com>
2025-07-19 21:04:56 +0300

    19.2. feat(PortfolioService): add initial EF Core relations between domain entities

---
+++ src/backend/services/PortfolioService/PortfolioService.Infrastructure.EntityFramework/Context/Configurations/PortfolioConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StockMarketAssistant.PortfolioService.Domain.Entities;
+
+namespace StockMarketAssistant.PortfolioService.Infrastructure.EntityFramework.Context.Configurations
+{
+    public class PortfolioConfiguration : IEntityTypeConfiguration<Portfolio>
+    {
+        public void Configure(EntityTypeBuilder<Portfolio> builder)
+        {
+            builder.HasMany(p => p.Assets)
+                .WithOne(a => a.Portfolio)
+                .HasForeignKey(a => a.PortfolioId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Property(p => p.UserId)
+                .IsRequired();
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(r => r.Currency)
+                .IsRequired()
+                .HasMaxLength(10);
+        }
+    }
+}
