2e47325d90af0c180cedd19cbd3cce61b9e10b2b
Igor Melnikov <iemelnikov@gmail.com>
2025-07-19 21:18:28 +0300

    19.3. feat(PortfolioService): add EF Core integration with PostgreSQL

---
+++ src/backend/services/PortfolioService/PortfolioService.Application/Interfaces/Repositories/IPortfolioRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using StockMarketAssistant.PortfolioService.Domain.Entities;
+
+namespace StockMarketAssistant.PortfolioService.Application.Interfaces.Repositories
+{
+    /// <summary>
+    /// Репозиторий работы с портфелями ценных бумаг
+    /// </summary>
+    public interface IPortfolioRepository: IRepository<Portfolio, Guid>
+    {
+    }
+}
---
+++ src/backend/services/PortfolioService/PortfolioService.Infrastructure.Repositories/PortfolioRepository.cs
@@ -0,0 +1,14 @@
+﻿using StockMarketAssistant.PortfolioService.Application.Interfaces.Repositories;
+using StockMarketAssistant.PortfolioService.Domain.Entities;
+using StockMarketAssistant.PortfolioService.Infrastructure.EntityFramework.Context;
+
+namespace StockMarketAssistant.PortfolioService.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Репозиторий EF Core для портфелей ценных бумаг
+    /// </summary>
+    /// <param name="dataContext"></param>
+    public class PortfolioRepository(DatabaseContext dataContext) : EfRepository<Portfolio, Guid>(dataContext), IPortfolioRepository
+    {
+    }
+}
