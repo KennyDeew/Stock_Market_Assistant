04eaa97f77fd4e728f3b6b7d718735675e8b721e
Igor Melnikov <iemelnikov@gmail.com>
2025-07-19 22:06:10 +0300

    20.1. feat(PortfolioService): add initial Dto classes for AppServices

---
+++ src/backend/services/PortfolioService/PortfolioService.Application/DTOs/UpdatingPortfolioAssetDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockMarketAssistant.PortfolioService.Application.DTOs
+{
+    /// <summary>
+    /// DTO для редактирования актива портфеля ценных бумаг
+    /// </summary>
+    public record UpdatingPortfolioAssetDto(int Quantity, decimal AveragePurchasePrice, DateTime LastUpdated, [MaxLength(10)] string Currency);
+}
---
+++ src/backend/services/PortfolioService/PortfolioService.Application/DTOs/UpdatingPortfolioDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockMarketAssistant.PortfolioService.Application.DTOs
+{
+    /// <summary>
+    /// DTO для редактирования портфеля ценных бумаг
+    /// </summary>
+    public record UpdatingPortfolioDto([MaxLength(100)] string Name, [MaxLength(10)] string Currency);
+}
