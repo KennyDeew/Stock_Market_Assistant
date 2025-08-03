869eef4bee870c95b98b2b74ca72362d1e1c1629
KennyDew <dubrovskiynv@gmail.com>
2025-08-03 23:24:43 +0300

    17.5 added Mapper for BondCard

---
+++ src/backend/services/StockCardService/StockCardService.WebApi/Mappers/BondCardMapper.cs
@@ -0,0 +1,123 @@
+﻿using StockCardService.WebApi.Models.BondCard;
+using StockMarketAssistant.StockCardService.Application.DTOs._02_BondCard;
+
+namespace StockMarketAssistant.StockCardService.WebApi.Mappers
+{
+    /// <summary>
+    /// маппер для карточки облигации Dto/Model
+    /// </summary>
+    public class BondCardMapper
+    {
+        public static BondCardDto ToDto(BondCardModel model)
+        {
+            if (model == null) return null;
+
+            return new BondCardDto
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Ticker = model.Ticker,
+                Description = model.Description,
+                MaturityPeriod = DateTime.SpecifyKind(model.MaturityPeriod, DateTimeKind.Utc),
+                Coupons = model.Coupons?.Select(CouponMapper.ToDto).ToList()
+            };
+        }
+
+        public static CreatingBondCardDto ToDto(CreatingBondCardModel model)
+        {
+            if (model == null) return null;
+
+            return new CreatingBondCardDto
+            {
+                Name = model.Name,
+                Ticker = model.Ticker,
+                Description = model.Description,
+                MaturityPeriod = DateTime.SpecifyKind(model.MaturityPeriod, DateTimeKind.Utc)
+            };
+        }
+        
+        public static UpdatingBondCardDto ToDto(UpdatingBondCardModel model)
+        {
+            if (model == null) return null;
+
+            return new UpdatingBondCardDto
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Ticker = model.Ticker,
+                Description = model.Description,
+                MaturityPeriod = DateTime.SpecifyKind(model.MaturityPeriod, DateTimeKind.Utc)
+            };
+        }
+
+        public static BondCardShortDto ToDto(BondCardShortModel model)
+        {
+            if (model == null) return null;
+
+            return new BondCardShortDto
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Ticker = model.Ticker,
+                Description = model.Description,
+                MaturityPeriod = model.MaturityPeriod.ToString()
+            };
+        }
+
+        public static BondCardModel ToModel(BondCardDto dto)
+        {
+            if (dto == null) return null;
+
+            return new BondCardModel
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                Ticker = dto.Ticker,
+                Description = dto.Description,
+                MaturityPeriod = dto.MaturityPeriod,
+                Coupons = dto.Coupons?.Select(CouponMapper.ToModel).ToList()
+            };
+        }
+
+        public static CreatingBondCardModel ToModel(CreatingBondCardDto dto)
+        {
+            if (dto == null) return null;
+
+            return new CreatingBondCardModel
+            {
+                Name = dto.Name,
+                Ticker = dto.Ticker,
+                Description = dto.Description,
+                MaturityPeriod = dto.MaturityPeriod
+            };
+        }
+
+        public static UpdatingBondCardModel ToModel(UpdatingBondCardDto dto)
+        {
+            if (dto == null) return null;
+
+            return new UpdatingBondCardModel
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                Ticker = dto.Ticker,
+                Description = dto.Description,
+                MaturityPeriod = dto.MaturityPeriod
+            };
+        }
+
+        public static BondCardShortModel ToModel(BondCardShortDto dto)
+        {
+            if (dto == null) return null;
+
+            return new BondCardShortModel
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                Ticker = dto.Ticker,
+                Description = dto.Description,
+                MaturityPeriod = dto.MaturityPeriod
+            };
+        }
+    }
+}
