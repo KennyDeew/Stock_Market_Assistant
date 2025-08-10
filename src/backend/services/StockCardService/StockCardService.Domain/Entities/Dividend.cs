7a26bfe0d42e3ae328e7822b4c1a8217103bfca1
KennyDew <dubrovskiynv@gmail.com>
2025-08-10 23:53:00 +0300

    changed Dividend.cs

--- src/backend/services/StockCardService/StockCardService.Domain/Entities/Dividend.cs
+++ src/backend/services/StockCardService/StockCardService.Domain/Entities/Dividend.cs
@@ -1,16 +1,45 @@
 ﻿using StockCardService.Domain.Entities;
-using System;
 
 namespace StockMarketAssistant.StockCardService.Domain.Entities
 {
+    /// <summary>
+    /// Класс дивидендов
+    /// </summary>
     public class Dividend : IEntity<Guid>
     {
+        /// <summary>
+        /// Id дивидендов
+        /// </summary>
         public Guid Id { get; set; }
+
+        /// <summary>
+        /// Id акции
+        /// </summary>
         public Guid ShareCardId { get; set; }
-        public DateTime Period { get; set; }
+
+        /// <summary>
+        /// дата див отсечки
+        /// </summary>
+        public DateTime CuttOffDate { get; set; }
+
+        /// <summary>
+        /// период выплаты
+        /// </summary>
+        public string Period { get; set; }
+
+        /// <summary>
+        /// Валюта
+        /// </summary>
         public string Currency { get; set; }
+
+        /// <summary>
+        /// Размер дивидендов
+        /// </summary>
         public decimal Value { get; set; }
-        
+
+        /// <summary>
+        /// Акция
+        /// </summary>
         public ShareCard ShareCard { get; set; }
     }
 }
