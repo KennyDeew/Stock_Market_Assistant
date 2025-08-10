007e54243966c1d00d10644374cf75f5efce7ffb
KennyDew <dubrovskiynv@gmail.com>
2025-08-10 23:53:46 +0300

    35.3 added DividendMapper (dto/model)

--- src/backend/services/StockCardService/StockCardService.WebApi/Mappers/DividendMapper.cs
+++ src/backend/services/StockCardService/StockCardService.WebApi/Mappers/DividendMapper.cs
@@ -11,7 +11,37 @@ namespace StockMarketAssistant.StockCardService.WebApi.Mappers
 
             return new DividendDto
             {
-                Id = model.Id
+                Id = model.Id,
+                ShareCardId = model.ShareCardId,
+                CuttOffDate = model.CuttOffDate,
+                Period = model.Period,
+                Currency = model.Currency,
+                Value = model.Value
+            };
+        }
+
+        public static CreatingDividendDto ToDto(CreatingDividendModel model)
+        {
+            if (model == null) return null;
+
+            return new CreatingDividendDto
+            {
+                ShareCardId = model.ShareCardId,
+                CuttOffDate = model.CuttOffDate,
+                Period = model.Period,
+                Currency = model.Currency,
+                Value = model.Value
+            };
+        }
+
+        public static UpdatingDividendDto ToDto(UpdatingDividendModel model)
+        {
+            if (model == null) return null;
+
+            return new UpdatingDividendDto
+            {
+                CuttOffDate = model.CuttOffDate,
+                Value = model.Value
             };
         }
 
@@ -21,11 +51,38 @@ namespace StockMarketAssistant.StockCardService.WebApi.Mappers
 
             return new DividendModel
             {
-                Id = dto.Id
+                Id = dto.Id,
+                ShareCardId = dto.ShareCardId,
+                CuttOffDate = dto.CuttOffDate,
+                Period = dto.Period,
+                Currency = dto.Currency,
+                Value = dto.Value
             };
         }
-         
-         
-         
+
+        public static CreatingDividendModel ToDto(CreatingDividendDto dto)
+        {
+            if (dto == null) return null;
+
+            return new CreatingDividendModel
+            {
+                ShareCardId = dto.ShareCardId,
+                CuttOffDate = dto.CuttOffDate,
+                Period = dto.Period,
+                Currency = dto.Currency,
+                Value = dto.Value
+            };
+        }
+
+        public static UpdatingDividendModel ToDto(UpdatingDividendDto dto)
+        {
+            if (dto == null) return null;
+
+            return new UpdatingDividendModel
+            {
+                CuttOffDate = dto.CuttOffDate,
+                Value = dto.Value
+            };
+        }
     }
 }
