d508d4acd21e9656e8a1ccbea7e6251c01a33c45
Максим <shadrin.maksim.sm@gmail.com>
2025-08-21 18:13:40 +0300

    Обновил проект AuthService.Domain Перенёс из своего репозитория. ✅

---
+++ src/backend/services/AuthService/src/AuthService.Domain/User.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthService.Domain;
+
+public class User : IdentityUser<Guid>
+{
+    private User()
+    {
+    }
+
+    private List<Role> _roles = [];
+
+    public IReadOnlyList<Role> Roles => _roles;
+
+    public static User CreateAdmin(string userName, string email, Role role)
+    {
+        return new User
+        {
+            UserName = userName,
+            Email = email,
+            _roles = [role],
+        };
+    }
+
+    public static User Create(string userName, string email)
+    {
+        return new User
+        {
+            UserName = userName,
+            Email = email,
+        };
+    }
+}
