d40f1f50014ec28b31c4549e5fda6614cca1ec8b
KennyDew <dubrovskiynv@gmail.com>
2025-08-31 23:00:52 +0300

    42. added MongoDbContext

---
+++ src/backend/services/StockCardService/StockCardService.Infrastructure.EntityFramework/MongoDBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using StockMarketAssistant.StockCardService.Infrastructure.EntityFramework.Settings;
+
+namespace StockMarketAssistant.StockCardService.Infrastructure.EntityFramework
+{
+    public class MongoDBContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDBContext(IOptions<MongoSettings> settings)
+        {
+            //клиент (один на приложение: singleton). Нужен для связи с сервером и работы с БД.
+            var client = new MongoClient(settings.Value.ConnectionString);
+            //по имени получаем БД
+            _database = client.GetDatabase(settings.Value.DatabaseName);
+        }
+
+        //короткая запись для свойства с геттером
+        public IMongoDatabase Database => _database;
+
+        public IMongoCollection<T> GetCollection<T>(string name = null)
+        {
+            var collectionName = name ?? typeof(T).Name + "s";
+            return _database.GetCollection<T>(collectionName);
+        }
+    }
+}
---
+++ src/backend/services/StockCardService/StockCardService.Infrastructure.EntityFramework/Settings/MongoSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace StockMarketAssistant.StockCardService.Infrastructure.EntityFramework.Settings
+{
+    public class MongoSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
