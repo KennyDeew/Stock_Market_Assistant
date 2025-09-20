00b5b1daee8f089f9c359d8fda68fa41da0cb5ca
drogremtal <bp240@outlook.com>
2025-09-20 13:44:48 +0300

    submodule

--- src/StockMarketAssistant.AppHost/Program.cs
+++ src/StockMarketAssistant.AppHost/Program.cs
@@ -5,15 +5,15 @@ internal class Program
     private static void Main(string[] args)
     {
         var builder = DistributedApplication.CreateBuilder(args);
-        
+
         // Добавление проектов
-        
+
         var apiGatewayService = builder.AddProject<Projects.Gateway_WebApi>("gateway-api");
         var apiAuthService = builder.AddProject<Projects.AuthService_WebApi>("authservice-api");
         var apiStockCardService = builder.AddProject<Projects.StockCardService_WebApi>("stockcardservice-api");
         var apiPortfolioService = builder.AddProject<Projects.PortfolioService_WebApi>("portfolioservice-api");
         var apiAnalyticsService = builder.AddProject<Projects.AnalyticsService_WebApi>("analyticsservice-api");
-        var apiNotificationService = builder.AddProject<Projects.NotificationService_WebApi>("notificationservice-api");
+        var apiNotificationService = builder.AddProject<Projects.NotificationService>("notificationservice-api");
 
         // Добавление ресурсов
         var redis = builder.AddRedis("cache");
@@ -28,6 +28,15 @@ internal class Program
             .WithHostPort(14051)
             .AddDatabase("stock-card-db");
 
+        var notificationPostgres = builder.AddPostgres("notification-db")
+            .WithImage("postgres:17.5")
+            .WithHostPort(14053).WithPgWeb(n => n.WithHostPort(5000))
+            .AddDatabase("notificationDb");
+
+        var kafka = builder.AddKafka("kafka")
+            .WithKafkaUI(kafka => kafka.WithHostPort(9100));
+
+
         //var postgres = builder.AddPostgres("postgres").AddDatabase("stockcarddb");
         //var container = builder.AddDockerfile("gateway", "../backend/gateway/");
 
@@ -39,6 +48,12 @@ internal class Program
         apiStockCardService.WithReference(redis)
                            .WithReference(pgStockCardDb)
                            .WaitFor(pgStockCardDb);
+
+        apiNotificationService
+            .WithReference(notificationPostgres)
+            .WithReference(kafka)
+            .WaitFor(notificationPostgres)
+            .WaitFor(kafka);
 
 
         builder.Build().Run();
