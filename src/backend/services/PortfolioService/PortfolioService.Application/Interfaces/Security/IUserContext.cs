c17bde39f0035662fcad5b44c22fd136035d13bb
Igor Melnikov <iemelnikov@gmail.com>
2025-11-17 19:55:45 +0300

    feat(PortfolioService): small fix

---
+++ src/backend/services/PortfolioService/PortfolioService.Application/Interfaces/Security/IUserContext.cs
@@ -0,0 +1,24 @@
+﻿namespace StockMarketAssistant.PortfolioService.Application.Interfaces.Security
+{
+    /// <summary>
+    /// Контекст текущего пользователя
+    /// </summary>
+    public interface IUserContext
+    {
+        /// <summary>
+        /// ID текущего пользователя из JWT
+        /// </summary>
+        Guid UserId { get; }
+
+        /// <summary>
+        /// Текущая роль
+        /// </summary>
+        string Role { get; }
+
+        /// <summary>
+        /// Является ли пользователь админом?
+        /// </summary>
+        bool IsAdmin => Role.Equals("ADMIN", StringComparison.OrdinalIgnoreCase);
+    }
+
+}
